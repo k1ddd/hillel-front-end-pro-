b98f4098ffb185e168540b9628d8b958a466749a
vladyslav <vladyslav.klokun@gmail.com>
2022-09-06 00:22:31 +0300

    create HW_2 branch and finish hw_2

---
+++ main.js
@@ -0,0 +1,35 @@
+let myName = "Vladyslav";
+
+let mySurmame = "Klokun";
+
+let myCity = "Kyiv";
+
+let average = 10 + 12 + 10 + 5 / 4;
+
+let circleRadius = 5;
+
+let length;
+
+let area;
+
+const PI = 3.14159265358979;
+
+length = 2 * PI * circleRadius; // calculate the length
+area = PI * circleRadius ** 2; // calculate the area
+
+/* let 5 = 5;  
+
+let this;
+
+let function = function; 
+// wrong variable
+*/
+
+console.log("my Name =", myName);
+console.log("my Surmame =", mySurmame);
+console.log("my City =", myCity);
+console.log("average =", average);
+console.log("circle radius = ", circleRadius);
+console.log("length = ", length);
+console.log("area = ", area);
+console.log("PI = ", PI);
