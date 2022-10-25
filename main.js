3109cfe2603784f7d86a91eb837a2289b581f3c1
vladyslav <vladyslav.klokun@gmail.com>
2022-10-26 00:08:52 +0300

    finish hW_11

--- main.js
+++ main.js
@@ -1,168 +1,269 @@
-function NewAuto() {
-  const info = { ...arguments[0] };
-  this.fuelConsumption = info[0];
-  this.engineType = info[1];
-  this.modelOfCar = info[2];
-  this.engineVolume = info[3];
-  this.year = info[4];
-  this.carOfMass = info[5];
-  this.status = info[6];
+const UA = `./UA.png`;
+const USA = `./USA.png`;
+const CHZ = `./CHZ.png`;
 
-  this.damageCar = 100;
+const productLists = [
+  {
+    productName: "Bread",
+    productData: {
+      weight: 200,
+      certificate: true,
+      dateOfExpiry: "27/10/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 11.75,
+  },
+  {
+    productName: "Water",
+    productData: {
+      weight: 1500,
+      certificate: true,
+      dateOfExpiry: "21/12/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 17,
+  },
+  {
+    productName: "Wine",
+    productData: {
+      weight: 750,
+      certificate: true,
+      dateOfExpiry: "21/12/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: USA,
+    productPrice: 150,
+  },
+  {
+    productName: "Apple",
+    productData: {
+      weight: 2000,
+      certificate: true,
+      dateOfExpiry: "11/01/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 18,
+  },
+  {
+    productName: "Flour",
+    productData: {
+      weight: 1000,
+      certificate: false,
+      dateOfExpiry: "10/10/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 80,
+  },
+  {
+    productName: "Egg",
+    productData: {
+      weight: 300,
+      certificate: true,
+      dateOfExpiry: "21/11/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 60,
+  },
+  {
+    productName: "Milk",
+    productData: {
+      weight: 1000,
+      certificate: true,
+      dateOfExpiry: "22/12/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: CHZ,
+    productPrice: 11.75,
+  },
+  {
+    productName: "Rice",
+    productData: {
+      weight: 1000,
+      certificate: true,
+      dateOfExpiry: "23/12/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 59,
+  },
+  {
+    productName: "Buckwheat",
+    productData: {
+      weight: 1000,
+      certificate: true,
+      dateOfExpiry: "15/12/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 70,
+  },
+  {
+    productName: "Bun",
+    productData: {
+      weight: 150,
+      certificate: true,
+      dateOfExpiry: "29/10/22",
+      sugarFree: false,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 25,
+  },
+  {
+    productName: "Tea",
+    productData: {
+      weight: 200,
+      certificate: false,
+      dateOfExpiry: "21/11/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: CHZ,
+    productPrice: 45.5,
+  },
+  {
+    productName: "Coffee",
+    productData: {
+      weight: 250,
+      certificate: false,
+      dateOfExpiry: "21/01/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 140,
+  },
+  {
+    productName: "Beer",
+    productData: {
+      weight: 550,
+      certificate: true,
+      dateOfExpiry: "11/02/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: UA,
+    productPrice: 51,
+  },
+  {
+    productName: "Seasoning",
+    productData: {
+      weight: 400,
+      certificate: true,
+      dateOfExpiry: "29/10/23",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: CHZ,
+    productPrice: 78.5,
+  },
+  {
+    productName: "Vegetables",
+    productData: {
+      weight: 3000,
+      certificate: true,
+      dateOfExpiry: "10/11/22",
+      sugarFree: true,
+    },
+    productProvider: "FOP Skyba R.R.",
+    productCountry: USA,
+    productPrice: 275,
+  },
+];
+
+const div = document.createElement("div");
+
+const ul = document.createElement("ul");
+document.body.append(div);
+div.append(ul);
+
+function groceryBasket(
+  productName,
+  productPrice,
+  certificate,
+  productCountry,
+  weight,
+  dateOfExpiry,
+  sugarFree,
+  productProvider
+) {
+  return `<div class ="product__item">
+   <span class = "name__product">${productName}</span>
+    <span> Price : ${productPrice} </span>
+    <span> Country <img src = "${productCountry}" alt = "IMG"> </span> 
+  </div>
+  <div>
+    Date of expiry: ${dateOfExpiry},  Sugar Free: ${sugarFree}, Product provider: ${productProvider}, Weight: ${weight}, 
+    Sertificate: ${certificate} <span class= "warning">${
+    certificate ? "" : "!"
+  }</span>
+  </div>`;
 }
 
-NewAuto.prototype.startRide = function () {
-  if (!this.status) {
-    console.log(`${this.modelOfCar} starting to ride`);
-    this.status = true;
-    this.damageCar = this.damageCar - carServiceability();
-  } else {
-    console.log(`${this.modelOfCar} still on the trip`);
-  }
-};
+productLists.forEach((product) => {
+  const {
+    productName,
+    productPrice,
+    productData,
+    productCountry,
+    productProvider,
+  } = product;
 
-NewAuto.prototype.endRide = function () {
-  this.status = false;
-  console.log(`${this.modelOfCar} is shutting  now. Bye-Bye.`);
-};
+  const li = document.createElement("li");
+  li.className = "product";
+  li.innerHTML = `${groceryBasket(
+    productName,
+    productPrice,
+    productData.certificate,
+    productCountry,
+    productData.weight,
+    productData.dateOfExpiry,
+    productData.sugarFree,
+    productProvider
+  )}`;
+  ul.append(li);
+});
 
-function carServiceability() {
-  return (Math.random() * (2.5 - 0.1) + 0.1).toFixed(2);
-}
-function Hatchback() {
-  NewAuto.call(this, arguments);
-}
-function Sedan() {
-  NewAuto.call(this, arguments);
-}
-function WagonCar() {
-  NewAuto.call(this, arguments);
-}
+const totalAmoutn = productLists.reduce(
+  (sumProduct, currentValue) => sumProduct + currentValue.productPrice,
+  0
+);
 
-Hatchback.prototype = Object.create(NewAuto.prototype);
-Sedan.prototype = Object.create(NewAuto.prototype);
-WagonCar.prototype = Object.create(NewAuto.prototype);
+const mostExpensive = productLists.sort(
+  (sumProduct, currentValue) =>
+    currentValue.productPrice - sumProduct.productPrice
+)[0];
 
-const ford = new Hatchback(5, "diesel", "Ford Fiesta", 1.4, 2006, 1100, "true");
-const toyota = new Sedan(15, "petrol", "Toyota Camry", 3.5, 2008, 1600, "true");
-const vw = new WagonCar(10, "petrol", "Vw Golf", 2, 2000, 1300, "true");
+const averagePriceOfAllProducts = +(totalAmoutn / productLists.length).toFixed(
+  1
+);
 
-vw.startRide();
-vw.endRide();
+const textData = [
+  `<span class="sum"> The total amount of your cart is : ${totalAmoutn}.</span>`,
+  `<span class="sum">The most expensive product on this list is: ${mostExpensive.productName} its price : ${mostExpensive.productPrice}.</span>`,
+  `<span class="sum"> Arage price of a grocery basket : ${averagePriceOfAllProducts}.</span>`,
+];
 
-function dealer(car) {
-  const prices = {};
-  let priceOfCoast = (100 - car.damageCar).toFixed(2);
-
-  if (car instanceof Hatchback) {
-    prices.typeHatchback = 110;
-
-    if (car.engineType === "diesel") {
-      prices.engineKoef = 2.2;
-    } else car.engineType === "petrol";
-    prices.engineKoef = 1.8;
-    if (car.year > 2019) {
-      prices.yearKoef = 1.2;
-    } else car.year < 2018 && car.year > 2010;
-    prices.yearKoef = 1.4;
-    if (car.year < 2009 && car.year > 2000) {
-      prices.yearKoef = 1.7;
-    } else car.year < 1999 && car.year > 1990;
-    prices.yearKoef = 2;
-    if (car.year < 1990) {
-      console.log(`Sorry , we don't repair cars until 1990  `);
-    }
-    if (car.carOfMass > 800 && car.carOfMass < 1100) {
-      prices.massKoef = 1.5;
-    } else if (car.carOfMass > 1101 && car.carOfMass < 1400) {
-      prices.massKoef = 1.7;
-    } else if (car.carOfMass > 1401) {
-      prices.massKoef = 2;
-    }
-
-    const amountYearHatchback = prices.yearKoef * prices.typeHatchback;
-    const amountEngineHatchback = prices.engineKoef * prices.typeHatchback;
-    const amountMassHatchback = prices.massKoef * prices.typeHatchback;
-    const totalAmount =
-      prices.typeHatchback * priceOfCoast +
-      amountYearHatchback +
-      amountEngineHatchback +
-      amountMassHatchback.toFixed(2);
-    return `Repairing for your ${car.modelOfCar} will cost ${totalAmount}`;
-  }
-  if (car instanceof Sedan) {
-    prices.typeSedan = 125;
-
-    if (car.engineType === "diesel") {
-      prices.engineKoef = 2.5;
-    } else car.engineType === "petrol";
-    prices.engineKoef = 2;
-    if (car.year > 2019) {
-      prices.yearKoef = 1.5;
-    } else car.year < 2018 && car.year > 2010;
-    prices.yearKoef = 1.8;
-    if (car.year < 2009 && car.year > 2000) {
-      prices.yearKoef = 2;
-    } else car.year < 1999 && car.year > 1990;
-    prices.yearKoef = 2.6;
-    if (car.year < 1990) {
-      console.log(`Sorry , we don't repair cars until 1990  `);
-    }
-    if (car.carOfMass > 800 && car.carOfMass < 1100) {
-      prices.massKoef = 1.6;
-    } else if (car.carOfMass > 1101 && car.carOfMass < 1400) {
-      prices.massKoef = 1.8;
-    } else if (car.carOfMass > 1401) {
-      prices.massKoef = 2.1;
-    }
-
-    const amountYearSedan = prices.yearKoef * prices.typeSedan;
-    const amountEngineSedan = prices.engineKoef * prices.typeSedan;
-    const amountMassSedan = prices.massKoef * prices.typeSedan;
-    const totalAmount =
-      prices.typeSedan * priceOfCoast +
-      amountYearSedan +
-      amountEngineSedan +
-      amountMassSedan.toFixed(2);
-    return `Repairing for your ${car.modelOfCar} will cost ${totalAmount}`;
-  }
-  if (car instanceof WagonCar) {
-    prices.typeWagonCar = 130;
-
-    if (car.engineType === "diesel") {
-      prices.engineKoef = 2.9;
-    } else car.engineType === "petrol";
-    prices.engineKoef = 2.4;
-    if (car.year > 2019) {
-      prices.yearKoef = 2;
-    } else car.year < 2018 && car.year > 2010;
-    prices.yearKoef = 2.2;
-    if (car.year < 2009 && car.year > 2000) {
-      prices.yearKoef = 2.5;
-    } else car.year < 1999 && car.year > 1990;
-    prices.yearKoef = 3;
-    if (car.year < 1990) {
-      console.log(`Sorry , we don't repair cars until 1990  `);
-    }
-    if (car.carOfMass > 800 && car.carOfMass < 1100) {
-      prices.massKoef = 1.7;
-    } else if (car.carOfMass > 1101 && car.carOfMass < 1400) {
-      prices.massKoef = 1.9;
-    } else if (car.carOfMass > 1401) {
-      prices.massKoef = 2.2;
-    }
-
-    const amountYearWagonCar = prices.yearKoef * prices.typeWagonCar;
-    const amountEngineWagonCar = prices.engineKoef * prices.typeWagonCar;
-    const amountMassWagonCar = prices.massKoef * prices.typeWagonCar;
-    const totalAmount =
-      prices.typeWagonCar * priceOfCoast +
-      amountYearWagonCar +
-      amountEngineWagonCar +
-      amountMassWagonCar.toFixed(2);
-    return `Repairing for your ${car.modelOfCar} will cost ${totalAmount}`;
-  }
-  this.damageCar = 100;
+function createAndAddToInnerHTML() {
+  textData.forEach((element) => {
+    const h4 = document.createElement("h4");
+    h4.innerHTML = element;
+    div.append(h4);
+  });
 }
 
-dealer(vw);
+createAndAddToInnerHTML();
