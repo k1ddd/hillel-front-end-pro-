1db501f25fd15fe375647bcd568ad84f9b62810a
vladyslav <vladyslav.klokun@gmail.com>
2022-11-27 21:56:06 +0200

    finish hW_17

--- main.js
+++ main.js
@@ -1,54 +1,85 @@
-const apiKey = "EtabGNTDzqYExiuCoCeB2oFE0FXu3bZG";
-const city = document.querySelector("#select__city");
-const showMeTheWeather = document.querySelector("#btn");
-const p = document.querySelector("#weather");
-const weatherOfTheCity = {
-  Lviv: 324561,
-  Kyiv: 324505,
-  Kharkiv: 323903,
-  Chernihiv: 322162,
-  Odesa: 325343,
-  Mariupol: 323037,
-  Poltava: 325523,
-  Zhytomyr: 326609,
-  Cherkasy: 321985,
-  London: 328328,
-  Warsaw: 274663,
-  "New York": 349727,
-};
+const apiKey = "CkSNAVKzAqdXRuhLbFx9HOy2rXehD6KA";
 
-function loadData() {
-  const xhttp = new XMLHttpRequest();
+const city = document.getElementById("select__city");
+const showMeTheWeather = document.getElementById("btn");
+const p = document.getElementById("weather");
+const neighbors = document.getElementById("neighbors");
 
-  xhttp.open(
-    "GET",
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${
-      weatherOfTheCity[city.value]
-    }?apikey=${apiKey}`
-  );
-  xhttp.send();
+async function idCity(city) {
+  const URL = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
 
-  xhttp.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const result = JSON.parse(this.response);
-      p.innerHTML = `<h1> ${city.value}</h1>`;
-      result.DailyForecasts.forEach((value) => {
-        const tempMin = Math.round(
-          ((value.Temperature.Minimum.Value - 32) * 5) / 9
-        );
-        const tempMax = Math.round(
-          ((value.Temperature.Maximum.Value - 32) * 5) / 9
-        );
-        const date = new Date(value.Date);
-        p.innerHTML += `<span class="date">${date.toLocaleDateString(
-          "en-UK"
-        )}</span><p class="temp"> ${tempMin}°... ${tempMax}° </p><h4 class="cloudiness"> <span class="day__night">Day:</span> ${
-          value.Day.IconPhrase
-        }</h4> <h4 class="cloudiness"><span class="day__night">Night:</span> ${
-          value.Night.IconPhrase
-        }</h4>`;
-      });
-    } else alert("Error, invalid status, please check");
-  };
+  const resultJson = await (await fetch(URL)).json();
+  return resultJson[0].Key;
 }
-showMeTheWeather.addEventListener("click", loadData);
+
+function neighborsInfo(event) {
+  searchTheWeather(event.target.dataset.cityId, event.target.dataset.cityName);
+}
+
+async function searchTheWeather(cityId, cityName) {
+  if (!cityId) {
+    p.innerHTML = p.innerHTML = `<h2>Wrong city</h2>`;
+    return;
+  }
+  const forcastSearch = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityId}?apikey=${apiKey}&metric=true;`;
+  try {
+    const weather = await (await fetch(forcastSearch)).json();
+    const forecaststDaly = document.createElement("div");
+    forecaststDaly.classList.add("info__forcast");
+
+    p.innerHTML = `<h1> ${cityName}</h1>`;
+    weather.DailyForecasts.forEach((value) => {
+      const tempMin = Math.round(
+        ((value.Temperature.Minimum.Value - 32) * 5) / 9
+      );
+      const tempMax = Math.round(
+        ((value.Temperature.Maximum.Value - 32) * 5) / 9
+      );
+      const date = new Date(value.Date);
+      p.innerHTML += `<span class="date">${date.toLocaleDateString(
+        "en-UK"
+      )}</span><p class="temp"> ${tempMin}°... ${tempMax}° </p><h4 class="cloudiness"> <span class="day__night">Day:</span> ${
+        value.Day.IconPhrase
+      }</h4> <h4 class="cloudiness"><span class="day__night">Night:</span> ${
+        value.Night.IconPhrase
+      }</h4>`;
+    });
+  } catch (error) {
+    p.innerHTML = `<h4>Error in requested weather </h4>`;
+    return;
+  }
+
+  const locationNeighbors = `https://dataservice.accuweather.com/locations/v1/cities/neighbors/${cityId}?apikey=${apiKey}`;
+  try {
+    const citiesNearby = await (await fetch(locationNeighbors)).json();
+
+    const blockNeighbors = document.createElement("div");
+    blockNeighbors.classList.add("block__neighbors");
+
+    citiesNearby.forEach((el) => {
+      const neighborValue = document.createElement("button");
+      neighborValue.classList.add("neighbor__city");
+      neighborValue.dataset.cityId = el.Key;
+      neighborValue.dataset.cityName = el.LocalizedName;
+      neighborValue.innerText = el.LocalizedName;
+      neighborValue.addEventListener("click", neighborsInfo);
+      blockNeighbors.appendChild(neighborValue);
+    });
+
+    neighbors.innerHTML = `<h2>Neighboring cities with: ${cityName}</h2>`;
+    neighbors.appendChild(blockNeighbors);
+  } catch (error) {
+    p.innerHTML = `Error wheather of this city`;
+    return;
+  }
+}
+
+showMeTheWeather.addEventListener("click", async () => {
+  const cityName = city.value;
+  if (cityName === "") {
+    p.innerHTML = `<h2>Wrong city, please correct city</h2>`;
+    return;
+  }
+  const cityId = await idCity(cityName);
+  searchTheWeather(cityId, cityName);
+});
