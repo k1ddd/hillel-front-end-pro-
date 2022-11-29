7cef3bb8686720c1a4d67d649f8f9aefecf85dca
vladyslav <vladyslav.klokun@gmail.com>
2022-11-29 23:12:32 +0200

    finish hW_18

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const apiKey = "6d29bbab";
+const API_KEY = "6d29bbab";
 
 document.getElementById("form-submit").addEventListener("submit", getIp);
 let page = 1;
@@ -10,23 +10,23 @@ function getIp(e) {
 }
 
 const renderMovieNotFound = (error = "Movie not found!") => {
-  const movieNotFoundContainer = document.createElement("div");
-  movieNotFoundContainer.innerHTML = confirm(error);
+  let notFoundDiv = "";
+  notFoundDiv += `<div id="results"><p class="not__found">${error}</p></div>`;
+  document.getElementById("results").innerHTML = notFoundDiv;
 };
 
-function search(ip, page) {
-  let url = `https://www.omdbapi.com/?s=${ip}&page=${page}&apikey=${apiKey}`;
+async function search(ip, page) {
+  const url = `https://www.omdbapi.com/?s=${ip}&page=${page}&apikey=${API_KEY}`;
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.Response === "False") {
-        renderMovieNotFound(data.Error);
-      } else {
-        let output = "";
-        data.Search.forEach((result) => {
-          let poster = result.Poster ? result.Poster : "";
-          output += `<div class="col-md-3">
+  const response = await axios.get(url);
+  const data = response.data;
+  if (data.Response === "False") {
+    renderMovieNotFound(data.Error);
+  } else {
+    let output = "";
+    data.Search.forEach((result) => {
+      let poster = result.Poster ? result.Poster : "";
+      output += `<div class="col-md-3">
           <div class="card">
             <img class="card-img-top" src="${poster}" alt="Poster">
             <div class="card-body">
@@ -35,22 +35,21 @@ function search(ip, page) {
             </div>
           </div>
            </div>`;
-        });
-        output += `
+    });
+    output += `
       <div class="container mt-5 moviesInfo">
         <div class="row">
       <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item" id="previous"><a class="page-link" onclick="previous()">Previous</a></li>
-        <li class="page-item" id="next"><a class="page-link" onclick="next()">Next</a></li>
+        <li class="page-item"><a class="page-link" onclick="previous()">Previous</a></li>
+        <li class="page-item"><a class="page-link" onclick="next()">Next</a></li>
       </ul>
       </nav>
       </div>
       </div>
           `;
-        document.getElementById("results").innerHTML = output;
-      }
-    });
+    document.getElementById("results").innerHTML = output;
+  }
 }
 
 function next() {
@@ -62,12 +61,10 @@ function next() {
 
 function previous() {
   let iVal = searchWord.value;
-  if (page != 1) {
+  if (page > 2) {
     document.getElementById("results").innerHTML = "";
     page--;
     search(iVal, page);
-  } else {
-    page = 1;
   }
 }
 
@@ -97,26 +94,26 @@ const renderDetails = (data) => {
   const span_director = document.createElement("span");
   const span_box_office = document.createElement("span");
   posterImg.src = Poster;
-  paragraph.innerHTML = ` Plot:<span class="plot"> ${Plot} </span>
+  paragraph.innerHTML = ` <div class="movieDetails" id="more">
+  Plot:<span class="plot"> ${Plot} </span>
   <p>Director:<span class="director"> ${Director}</span></p>
   <p>BoxOffice:<span class="boxOffice"> ${BoxOffice} </span></p>
   <p>IMDB:<span class="IMDB"> ${imdbRating} / 10 </span></p>
   <p>Runtime:<span class="Runtime"> ${Runtime} </span></p>
   <p>Genre:<span class="Genre"> ${Genre} </span></p>
   <p>Actors:<span class="Actors"> ${Actors} </span></p>
-  <p>Country:<span class="Country"> ${Country} </span></p>`;
+  <p>Country:<span class="Country"> ${Country} </span></p></div>`;
   div.classList.add("movieDetails");
   moviesSection.append(posterImg, paragraph, span_director, span_box_office);
 };
 
-const showMore = (event) => {
+const showMore = async (event) => {
   if (event.target && event.target.id) {
-    fetch(`https://www.omdbapi.com/?i=${event.target.id}&apikey=${apiKey}`)
-      .then((response) => response.json())
-      .then((details) => {
-        console.log(details);
-        renderDetails(details);
-      });
+    const response = await axios.get(
+      `https://www.omdbapi.com/?i=${event.target.id}&apikey=${API_KEY}`
+    );
+    const details = response.data;
+    renderDetails(details);
   }
 };
 
